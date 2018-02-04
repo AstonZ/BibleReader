aad0dd7c57d7590dd62784cd9898b74baf818dfa
Aston K Mac <zhangliang203@pingan.com.cn>
2018-02-04 10:45:49 +0800

    native biblereader

---
+++ native/BibleReader/TheBibleReader/TheBibleReader/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TheBibleReader
+//
+//  Created by Aston K Mac on 2018/1/2.
+//  Copyright © 2018年 LowBee Tech. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
