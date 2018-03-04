5d567ad4e6d2664e11e35185a61b80fcfb24b943
Aston K Mac <zhangliang203@pingan.com.cn>
2018-03-04 16:48:44 +0800

    - Translation WebView Done

--- v1/BibleReader/index.js
+++ v1/BibleReader/index.js
@@ -1,4 +1,12 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import {StackNavigator} from 'react-navigation';
+
+import LoginPage from './src/screens/LoginPage'
+import RegisterPage from './src/screens/RegisterPage'
+
+const App = StackNavigator({
+  Login: {screen: LoginPage},
+  Register: {screen: RegisterPage}
+})
 
 AppRegistry.registerComponent('BibleReader', () => App);
